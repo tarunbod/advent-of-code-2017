ee5791b1263c80be0646c3e1487c7e85f4c27f64
Tarun Boddupalli <tarunbod@gmail.com>
2017-12-12 20:47:38 -0500

    zort

---
+++ d10p1.py
@@ -0,0 +1,16 @@
+numbers = [x for x in range(256)]
+lengths = [76,1,88,148,166,217,130,0,128,254,16,2,130,71,255,229]
+
+current = 0
+skip = 0
+for l in lengths:
+    pass
+    # if l > len(list): continue
+
+    # sample = list[current:(current+l)] + list[0:(current+l)%len(list)]
+    # sample.reverse()
+    # from i = current to current + l:
+    # do list[i % length] = sample[i]
+
+    # current += (l + skip) % len(list)
+    # skip += 1
